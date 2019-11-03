eb169730ae2e9e914d9997ac62693cdeb58853c9
egor <zzwerruga@mail.ru>
2019-11-03 23:36:10 +0300

    Code coverage added

--- build/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/egor/C_projects/RegexpParser/googletest-master/googlemock
-# Build directory: /home/egor/C_projects/RegexpParser/build/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/egor/C_projects/RegexpParser/googletest-master/googletest
-# Build directory: /home/egor/C_projects/RegexpParser/build/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
